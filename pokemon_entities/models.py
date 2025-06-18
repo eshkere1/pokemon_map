85210c1877531659871748780cf397d1622ed6f3
Pavel <piv17032009@gmail.com>
2025-06-18 10:59:47 +0300

    add PokemonEntity model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,5 +4,9 @@ class Pokemon(models.Model):
     title = models.CharField(max_length=200, verbose_name="Название покемона")
     photo = models.ImageField(null=True, blank=True, verbose_name="изображение покемона")
 
+class PokemonEntity(models.Model):
+    latitude = models.FloatField(verbose_name="Широта нахождения покемона")
+    longetude = models.FloatField(verbose_name="Долгота нахождения покемона")
+
     def __str__(self):
         return self.title
