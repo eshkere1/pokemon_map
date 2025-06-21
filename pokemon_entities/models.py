6fb4b9691e19193ed53f09b608a5191f839f2f62
Pavel <piv17032009@gmail.com>
2025-06-21 13:17:59 +0300

    add specs to PokemonEntity model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -5,17 +5,17 @@ class Pokemon(models.Model):
     photo = models.ImageField(null=True, blank=True, verbose_name="изображение покемона")
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    pokemon = models.ForeignKey(Pokemon, null=True, on_delete=models.CASCADE)
     latitude = models.FloatField(verbose_name="Широта нахождения покемона")
     longetude = models.FloatField(verbose_name="Долгота нахождения покемона")
-    appeared_at = models.DateTimeField(verbose_name="время и дата появления покемона")
-    disappeared_at = models.DateTimeField(verbose_name="время и дата исчезновения покемона")
-    level = models.IntegerField(verbose_name="уровень покемона")
-    health = models.IntegerField(verbose_name="здоровье")
-    strength = models.IntegerField(verbose_name="сила")
-    defence = models.IntegerField(verbose_name="защита")
-    stamina = models.IntegerField(verbose_name="выносливость")
+    appeared_at = models.DateTimeField(null=True, verbose_name="время и дата появления покемона")
+    disappeared_at = models.DateTimeField(null=True, verbose_name="время и дата исчезновения покемона")
+    level = models.IntegerField(null=True, verbose_name="уровень покемона")
+    health = models.IntegerField(null=True, verbose_name="здоровье")
+    strength = models.IntegerField(null=True, verbose_name="сила")
+    defence = models.IntegerField(null=True, verbose_name="защита")
+    stamina = models.IntegerField(null=True, verbose_name="выносливость")
 
-    
+
     def __str__(self):
         return self.title
