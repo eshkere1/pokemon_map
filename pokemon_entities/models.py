dba2b2cd2a8e1112f75ac764f51c87236b3eec43
Pavel <piv17032009@gmail.com>
2025-06-24 18:58:48 +0300

    show pokemons on mainpage

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,5 +1,6 @@
 from django.db import models  # noqa F401
 
+
 class Pokemon(models.Model):
     title = models.CharField(max_length=200, verbose_name="Название покемона")
     photo = models.ImageField(null=True, blank=True, verbose_name="изображение покемона")
